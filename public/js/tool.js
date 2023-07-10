20b22d4b5d55dc5fce2ff930e1e31abe5373c52d
Pritam Singh Solanki <65842693+Pritamsinghsolanki@users.noreply.github.com>
2023-07-10 23:21:27 +0530

    Update tool.js

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,5 +1,5 @@
 // connect to socket server
-const socket = io.connect("http://localhost:3000/");
+const socket = io.connect("https://myrealtimeboard.vercel.app/");
 // *********************************Basic Setup
 const board = document.querySelector(".board");
 board.height = window.innerHeight;
